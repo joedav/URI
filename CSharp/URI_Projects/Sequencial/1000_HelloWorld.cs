1ce60faae10a19649dba626088cc0a6f9502a240
Joelyson David <joelyson.david@live.com>
2021-08-25 23:09:30 -0300

    1000 Hello World!

---
+++ CSharp/URI_Projects/Sequencial/1000_HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace URI_Projects
+{
+    class HelloWorld_1000
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Java/src/Iniciante/J1000_HelloWorld.java
@@ -0,0 +1,5 @@
+public class J1000_HelloWorld{
+  public static void main(String[] args){
+    System.out.println("Hello World!");
+  }
+}
