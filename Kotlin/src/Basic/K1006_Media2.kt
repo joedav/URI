2334b0e91f309a843b3476056a0c755ea9209f8d
Joelyson David <joelyson.david@live.com>
2021-11-19 21:54:25 -0300

    1006 Media2

---
+++ Kotlin/src/Basic/K1006_Media2.kt
@@ -0,0 +1,32 @@
+package Basic
+
+fun main(args: Array<String>) {
+    val numberA = readLine()?.toDoubleOrNull() ?: -1.0
+    val numberB = readLine()?.toDoubleOrNull() ?: -1.0
+    val numberC = readLine()?.toDoubleOrNull() ?: -1.0
+
+    if (!isValidNumber(numberA, numberB, numberC)) return
+
+    val media = media2(numberA, numberB, numberC)
+
+    println("MEDIA = ${"%.1f".format(media)}")
+}
+
+private fun media2(number1: Double, number2: Double, number3: Double): Double {
+    val value1 = weightCalculate(number1, 2.0)
+    val value2 = weightCalculate(number2, 3.0)
+    val value3 = weightCalculate(number3, 5.0)
+    return (value1 + value2 + value3) / 10
+}
+
+private fun weightCalculate(value: Double, weight: Double): Double {
+    return value * weight
+}
+
+private fun isValidNumber(number1: Double, number2: Double, number3: Double): Boolean {
+    return isValidNumber(number1) && isValidNumber(number2) && isValidNumber(number3)
+}
+
+private fun isValidNumber(number: Double): Boolean {
+    return ((number > 0) && (number < 100))
+}
