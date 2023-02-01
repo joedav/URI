44bca9c745c1bec1386b54b8a0a2654034ac8b0e
joedav <joelyson.david@live.com>
2023-01-31 23:27:28 -0300

    fix: move folder

--- JavaScript/Iniciante/1003_SomaSimples.js
+++
@@ -1,7 +0,0 @@
-// MODIFICAR O PROMPT() POR lines.shift() quando for enviar para o uri
-let a = parseInt(prompt());
-let b = parseInt(prompt());
-
-let soma = a + b;
-
-console.log("SOMA = " + soma);
